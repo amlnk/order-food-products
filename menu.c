e93a3ef30b623b870cddcb7e5504ee784e330bb6
Аліна Мельник <kn24-a.melnyk@nubipl.edu.ua>
2025-07-16 01:27:19 +0300

    Created file menu.c in which dishes were added with price-Melnyk

---
+++ menu.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include "menu.h"
+
+void showMenu() {
+    printf("\n=== MENU ===\n");
+    printf("1. Margherita Pizza - 120 UAH\n");
+    printf("2. Chicken Burger - 95 UAH\n");
+    printf("3. Sushi Set - 220 UAH\n");
+    printf("0. Back\n");
+}
