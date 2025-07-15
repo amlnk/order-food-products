93422e0bc8b4bd6d5c834bce938dc1c48454bec8
Аліна Мельник <kn24-a.melnyk@nubipl.edu.ua>
2025-07-16 01:41:56 +0300

    Aded new dishes-Melnyk

--- menu.c
+++ menu.c
@@ -6,5 +6,8 @@ void showMenu() {
     printf("1. Margherita Pizza - 120 UAH\n");
     printf("2. Chicken Burger - 95 UAH\n");
     printf("3. Sushi Set - 220 UAH\n");
+    printf("4. Caesar Salad - 85 UAH\n");
+    printf("5. Spaghetti Carbonara - 150 UAH\n");
+    printf("6. Grilled Salmon - 200 UAH\n");
     printf("0. Back\n");
 }
